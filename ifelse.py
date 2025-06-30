835d9b0ce0eb877324af5094568ce127e3acb871
DeVenom3 <elonwong118@gmail.com>
2025-06-30 17:02:26 +0800

    ifelse.py
    上传第一个github应用

---
+++ ifelse.py
@@ -0,0 +1,22 @@
+"""
+#年龄18
+age = 18
+if age >= 18:
+    print("你可以进入网站")
+else:
+    print("未成年人禁止访问")
+"""
+
+#大于等于90优秀
+#大于等于79良好
+#其他及格以下
+
+score =100
+if score ==100:
+    print("特别奖励")
+elif score >= 90:
+    print("优秀")
+elif score >= 70:
+    print("良好")
+else:
+    print("及格以下")
